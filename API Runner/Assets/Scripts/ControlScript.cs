3afb56002dd386f4ecaa3d53073895a42a188524
LAPTOP\Jarno van Lierop <jarnovanlierop@gmail.com>
2018-10-12 11:52:15 +0200

    added2

---
+++ API Runner/Assets/Scripts/ControlScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ControlScript : MonoBehaviour {
+
+    public void Quit() {
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#elif UNITY_WEBPLAYER
+                         Application.OpenURL(webplayerQuitURL);
+#else
+                         Application.Quit();
+#endif
+    }
+
+    public void NextScene()
+	{
+		SceneManager.LoadScene("Game");
+	}
+}
